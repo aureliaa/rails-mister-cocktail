15b9da2221ad34fdcf59d0a7da1dbcb588caaf85
Aurelia Amalvict <aurelia.amalvict@gmail.com>
2017-10-03 12:34:56 +0200

    image upload fetaure

---
+++ app/uploaders/photo_uploader.rb
@@ -0,0 +1,18 @@
+# encoding: utf-8
+
+class PhotoUploader < CarrierWave::Uploader::Base
+
+include Cloudinary::CarrierWave
+
+  process eager: true
+  process convert: 'jpg'
+
+  version :standard do
+    resize_to_fit 800, 600
+  end
+
+  version :thumbnail do
+    resize_to_fit 200, 150
+  end
+
+end
