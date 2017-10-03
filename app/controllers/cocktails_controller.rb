2b1b189da08720a4581fb36113462be902a74c8c
Aurelia Amalvict <aurelia.amalvict@gmail.com>
2017-10-03 17:41:04 +0200

    with reviews

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -7,6 +7,7 @@ class CocktailsController < ApplicationController
     @cocktail = Cocktail.find(params[:id])
     @dose = Dose.new
     @ingredients = Ingredient.all
+    @review = Review.new
   end
 
   def new
